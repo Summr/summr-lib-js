a8917706237b83dbaabc339ff85f17608120434e
Ty Eggen <tycho.eggen@gmail.com>
2018-03-12 23:41:50 -0700

    thumbprint

---
+++ SOURCES.md
@@ -0,0 +1,9 @@
+# Library Sources
+
+## Description
+
+External sources get added to this document with a linkback to the original library when relevant
+
+## Thumbprint
+
+* https://github.com/leandrob/thumbprint/
---
+++ thumbprint.js
@@ -0,0 +1,15 @@
+/*
+ *
+ * Based on https://github.com/leandrob/thumbprint/
+ *
+ */
+var crypto = require('crypto');
+
+module.exports.calculate = function (cert,algo) {
+	if( algo == undefined ) { algo = 'sha1'; }
+
+	var shasum = crypto.createHash(algo);
+	var der = new Buffer(cert, 'base64').toString('binary')
+	shasum.update(der);
+	return shasum.digest('hex');
+}
